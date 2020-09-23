0760dabe155ce4ea16bb2387ef96f51f1bec3431
lucas cavalcante <lucas090163@gmail.com>
2020-09-23 10:13:14 -0400

    publish website

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ module.exports = {
     dialect: 'mysql',
     host:'mysql669.umbler.com',
     port:'41890',
-    username: 'luck133',
-    password:'Lucas123456',
-    database: 'lucas_1',
+    username: 'cesarino',
+    password:'CesarinoFilho123',
+    database: 'tavares_dados',
     define:{
         timestamps: true,
         underscored:true,
